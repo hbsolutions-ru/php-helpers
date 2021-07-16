1c8f22018a8659c0faa288c3b0ef67f8b666d1e1
Igor Losev <igor.losev@gmail.com>
2021-07-16 20:05:20 +0700

    ObjectHelper: add castWithPublicProperties method.

--- src/ObjectHelper.php
+++ src/ObjectHelper.php
@@ -21,4 +21,26 @@ final class ObjectHelper
             return self::toArray($object, $mutedFields);
         }, $objects);
     }
+
+    /**
+     * @param $object
+     * @param string $className
+     * @return mixed
+     * @throws \ReflectionException
+     */
+    public static function castWithPublicProperties($object, string $className)
+    {
+        $reflectionClass = new \ReflectionClass($className);
+        $properties = $reflectionClass->getProperties();
+        $result = $reflectionClass->newInstance();
+
+        foreach($properties as $property) {
+            $propertyName = $property->getName();
+            if ($property->isPublic() && isset($object->{$propertyName})) {
+                $property->setValue($result, $object->{$propName});
+            }
+        }
+
+        return $result;
+    }
 }
