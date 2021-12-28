eab4b1b9feca1ccca0735c30c51d67b1354b25b8
Igor Losev <igor.losev@gmail.com>
2021-12-28 13:35:25 +0700

    [UNIT-TESTS] DateTimeHelper: cover with unit tests.

--- tests/DateTimeHelperTest.php
+++ tests/DateTimeHelperTest.php
@@ -26,9 +26,14 @@ final class DateTimeHelperTest extends TestCase
         $parts = DateTimeHelper::parseUtcTimeOffset('UTC-05:00');
 
         $this->assertCount(3, $parts);
+
         $this->assertArrayHasKey(DateTimeHelper::UTC_TIME_OFFSET_SIGN, $parts);
         $this->assertArrayHasKey(DateTimeHelper::UTC_TIME_OFFSET_HOURS, $parts);
         $this->assertArrayHasKey(DateTimeHelper::UTC_TIME_OFFSET_MINUTES, $parts);
+
+        $this->assertEquals('-', $parts[DateTimeHelper::UTC_TIME_OFFSET_SIGN]);
+        $this->assertEquals(5, $parts[DateTimeHelper::UTC_TIME_OFFSET_HOURS]);
+        $this->assertEquals(0, $parts[DateTimeHelper::UTC_TIME_OFFSET_MINUTES]);
     }
 
     public function testUtcTimeOffsetToSeconds(): void
@@ -37,5 +42,33 @@ final class DateTimeHelperTest extends TestCase
             -19800,
             DateTimeHelper::utcTimeOffsetToSeconds('UTC-05:30')
         );
+    }
+
+    public function testValidateFormat(): void
+    {
+        $this->assertTrue(
+            DateTimeHelper::validateFormat('2021-12-02 12:34:56', 'Y-m-d H:i:s')
+        );
+
+        $this->assertFalse(
+            DateTimeHelper::validateFormat('2021-12-02T12:34:56', 'Y-m-d H:i:s')
+        );
+    }
+
+    public function testValidateUtcTimeOffset(): void
+    {
+        $positive = ['UTC±00:00', 'UTC+00:00', 'UTC-00:00', 'UTC-05:00', 'UTC+10:45'];
+        foreach ($positive as $utcTimeOffset) {
+            $this->assertTrue(
+                DateTimeHelper::validateUtcTimeOffset($utcTimeOffset)
+            );
+        }
+
+        $negative = ['UTC±00:01', 'UTC-20:00', 'UTC+99:99'];
+        foreach ($negative as $utcTimeOffset) {
+            $this->assertFalse(
+                DateTimeHelper::validateUtcTimeOffset($utcTimeOffset)
+            );
+        }
     }
 }
