3b9897c6cc663b0896ab0d67c33a01520e51e8e0
Igor Losev <igor.losev@gmail.com>
2022-02-02 21:56:29 +0700

    [UNIT-TESTS] ObjectHelper: cover 'objectsToArray' method.

--- tests/ObjectHelperTest.php
+++ tests/ObjectHelperTest.php
@@ -29,4 +29,38 @@ final class ObjectHelperTest extends TestCase
         $this->assertEquals($object->middleName, $array['middleName']);
         $this->assertEquals($object->age, $array['age']);
     }
+
+    public function testObjectsToArray(): void
+    {
+        $object1 = new \stdClass();
+        $object1->firstName = 'John';
+        $object1->lastName = 'Doe';
+        $object1->middleName = null;
+        $object1->age = 25;
+
+        $object2 = new \stdClass();
+        $object2->firstName = 'Jane';
+        $object2->lastName = 'Doe';
+        $object2->middleName = null;
+        $object2->age = 23;
+
+        $object3 = new \stdClass();
+        $object3->firstName = 'John';
+        $object3->lastName = 'Smith';
+        $object3->middleName = 'Jack';
+        $object3->age = 30;
+
+        $array = ObjectHelper::objectsToArray([$object1, $object2, $object3]);
+
+        $this->assertCount(3, $array);
+
+        $this->assertIsArray($array[0]);
+        $this->assertCount(4, $array[0]);
+
+        $this->assertIsArray($array[1]);
+        $this->assertCount(4, $array[1]);
+
+        $this->assertIsArray($array[2]);
+        $this->assertCount(4, $array[2]);
+    }
 }
