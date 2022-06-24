7878f1cff0a3fce1b772dc7660a9e19861efa6a6
Igor Losev <igor.losev@gmail.com>
2022-06-24 15:00:21 +0700

    [FEATURE] ObjectHelper: new helper creates new object and hydrates its properties with provided data from an array.

--- src/ObjectHelper.php
+++ src/ObjectHelper.php
@@ -4,28 +4,10 @@ namespace HBS\Helpers;
 
 final class ObjectHelper
 {
-    public static function toArray($object, array $mutedFields = []): array
-    {
-        $array = json_decode(json_encode($object), true);
-        foreach ($mutedFields as $field) {
-            if (array_key_exists($field, $array)) {
-                unset($array[$field]);
-            }
-        }
-        return $array;
-    }
-
-    public static function objectsToArray(array $objects, array $mutedFields = [])
-    {
-        return array_map(function ($object) use ($mutedFields) {
-            return self::toArray($object, $mutedFields);
-        }, $objects);
-    }
-
     /**
      * @param $object
      * @param string $className
-     * @return mixed
+     * @return object
      * @throws \ReflectionException
      */
     public static function castWithPublicProperties($object, string $className)
@@ -52,5 +34,55 @@ final class ObjectHelper
     public static function extractIds(array $objects, string $propertyName = 'id'): array
     {
         return array_map('intval', array_column($objects, $propertyName));
+    }
+
+    /**
+     * @param array $fieldsMap
+     * @param string $className
+     * @return object
+     * @throws \ReflectionException
+     */
+    public static function hydrateFromArray(array $fieldsMap, string $className)
+    {
+        $reflectionClass = new \ReflectionClass($className);
+        $properties = $reflectionClass->getProperties();
+        $result = $reflectionClass->newInstance();
+
+        foreach($properties as $property) {
+            $propertyName = $property->getName();
+            if ($property->isPublic() && isset($fieldsMap[$propertyName])) {
+                $property->setValue($result, $fieldsMap[$propertyName]);
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param array $objects
+     * @param string[] $mutedFields
+     * @return array
+     */
+    public static function objectsToArray(array $objects, array $mutedFields = []): array
+    {
+        return array_map(function ($object) use ($mutedFields) {
+            return self::toArray($object, $mutedFields);
+        }, $objects);
+    }
+
+    /**
+     * @param $object
+     * @param string[] $mutedFields
+     * @return array
+     */
+    public static function toArray($object, array $mutedFields = []): array
+    {
+        $array = json_decode(json_encode($object), true);
+        foreach ($mutedFields as $field) {
+            if (array_key_exists($field, $array)) {
+                unset($array[$field]);
+            }
+        }
+        return $array;
     }
 }
--- tests/ObjectHelperTest.php
+++ tests/ObjectHelperTest.php
@@ -12,69 +12,6 @@ use Tests\ObjectHelperData\{
 
 final class ObjectHelperTest extends TestCase
 {
-    public function testToArray(): void
-    {
-        $user = new User();
-        $user->id = 1;
-        $user->firstName = 'John';
-        $user->lastName = 'Doe';
-        $user->middleName = null;
-        $user->age = 25;
-
-        $array = ObjectHelper::toArray($user);
-
-        $this->assertCount(5, $array);
-
-        $this->assertArrayHasKey('id', $array);
-        $this->assertArrayHasKey('firstName', $array);
-        $this->assertArrayHasKey('lastName', $array);
-        $this->assertArrayHasKey('middleName', $array);
-        $this->assertArrayHasKey('age', $array);
-
-        $this->assertEquals($user->id, $array['id']);
-        $this->assertEquals($user->firstName, $array['firstName']);
-        $this->assertEquals($user->lastName, $array['lastName']);
-        $this->assertEquals($user->middleName, $array['middleName']);
-        $this->assertEquals($user->age, $array['age']);
-    }
-
-    public function testObjectsToArray(): void
-    {
-        $user1 = new User();
-        $user1->id = 1;
-        $user1->firstName = 'John';
-        $user1->lastName = 'Doe';
-        $user1->middleName = null;
-        $user1->age = 25;
-
-        $user2 = new User();
-        $user2->id = 2;
-        $user2->firstName = 'Jane';
-        $user2->lastName = 'Doe';
-        $user2->middleName = null;
-        $user2->age = 23;
-
-        $user3 = new User();
-        $user3->id = 3;
-        $user3->firstName = 'John';
-        $user3->lastName = 'Smith';
-        $user3->middleName = 'Jack';
-        $user3->age = 30;
-
-        $array = ObjectHelper::objectsToArray([$user1, $user2, $user3]);
-
-        $this->assertCount(3, $array);
-
-        $this->assertIsArray($array[0]);
-        $this->assertCount(5, $array[0]);
-
-        $this->assertIsArray($array[1]);
-        $this->assertCount(5, $array[1]);
-
-        $this->assertIsArray($array[2]);
-        $this->assertCount(5, $array[2]);
-    }
-
     public function testCastWithPublicProperties(): void
     {
         $user = new User();
@@ -121,5 +58,90 @@ final class ObjectHelperTest extends TestCase
 
         $this->assertEquals($order1->customerId, $customerIds[0]);
         $this->assertEquals($order2->customerId, $customerIds[1]);
+    }
+
+    public function testHydrateFromArray(): void
+    {
+        $data = [
+            'id' => 1,
+            'firstName' => 'John',
+            'lastName' => 'Doe',
+            'middleName' => null,
+            'age' => 25,
+        ];
+
+        /** @var User $user */
+        $user = ObjectHelper::hydrateFromArray($data, User::class);
+
+        $this->assertEquals(User::class, get_class($user));
+
+        $this->assertEquals($data['id'], $user->id);
+        $this->assertEquals($data['firstName'], $user->firstName);
+        $this->assertEquals($data['lastName'], $user->lastName);
+        $this->assertEquals($data['middleName'], $user->middleName);
+        $this->assertEquals($data['age'], $user->age);
+    }
+
+    public function testObjectsToArray(): void
+    {
+        $user1 = new User();
+        $user1->id = 1;
+        $user1->firstName = 'John';
+        $user1->lastName = 'Doe';
+        $user1->middleName = null;
+        $user1->age = 25;
+
+        $user2 = new User();
+        $user2->id = 2;
+        $user2->firstName = 'Jane';
+        $user2->lastName = 'Doe';
+        $user2->middleName = null;
+        $user2->age = 23;
+
+        $user3 = new User();
+        $user3->id = 3;
+        $user3->firstName = 'John';
+        $user3->lastName = 'Smith';
+        $user3->middleName = 'Jack';
+        $user3->age = 30;
+
+        $array = ObjectHelper::objectsToArray([$user1, $user2, $user3]);
+
+        $this->assertCount(3, $array);
+
+        $this->assertIsArray($array[0]);
+        $this->assertCount(5, $array[0]);
+
+        $this->assertIsArray($array[1]);
+        $this->assertCount(5, $array[1]);
+
+        $this->assertIsArray($array[2]);
+        $this->assertCount(5, $array[2]);
+    }
+
+    public function testToArray(): void
+    {
+        $user = new User();
+        $user->id = 1;
+        $user->firstName = 'John';
+        $user->lastName = 'Doe';
+        $user->middleName = null;
+        $user->age = 25;
+
+        $array = ObjectHelper::toArray($user);
+
+        $this->assertCount(5, $array);
+
+        $this->assertArrayHasKey('id', $array);
+        $this->assertArrayHasKey('firstName', $array);
+        $this->assertArrayHasKey('lastName', $array);
+        $this->assertArrayHasKey('middleName', $array);
+        $this->assertArrayHasKey('age', $array);
+
+        $this->assertEquals($user->id, $array['id']);
+        $this->assertEquals($user->firstName, $array['firstName']);
+        $this->assertEquals($user->lastName, $array['lastName']);
+        $this->assertEquals($user->middleName, $array['middleName']);
+        $this->assertEquals($user->age, $array['age']);
     }
 }
