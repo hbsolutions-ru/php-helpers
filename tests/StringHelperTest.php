559b1efadcc3bc17abf9091ede3d0aaafb6b8ee5
Igor Losev <igor.losev@gmail.com>
2022-08-22 18:57:24 +0700

    [UNIT-TESTS] StringHelper: cover 'kebabToCamelCase' method.

--- tests/StringHelperTest.php
+++ tests/StringHelperTest.php
@@ -16,6 +16,16 @@ final class StringHelperTest extends TestCase
         $this->assertEquals("the_quick_brown_fox_jumps_over_the_lazy_dog", $result);
     }
 
+    public function testComboKebabToSnakeCase(): void
+    {
+        $string = "the-quick-brown-fox-jumps-over-the-lazy-dog";
+
+        $camelCase = StringHelper::kebabToCamelCase($string);
+        $snakeCase = StringHelper::camelToSnakeCase($camelCase);
+
+        $this->assertEquals("the_quick_brown_fox_jumps_over_the_lazy_dog", $snakeCase);
+    }
+
     public function testGetLastPart(): void
     {
         $string = "The quick brown fox jumps over the lazy dog";
@@ -44,5 +54,14 @@ final class StringHelperTest extends TestCase
         $this->assertFalse(StringHelper::isNonEmptyString(42));
         $this->assertFalse(StringHelper::isNonEmptyString(3.1415926));
         $this->assertFalse(StringHelper::isNonEmptyString(["hello world"]));
+    }
+
+    public function testKebabToCamelCase(): void
+    {
+        $result = StringHelper::kebabToCamelCase("hello-world");
+        $this->assertEquals("helloWorld", $result);
+
+        $result = StringHelper::kebabToCamelCase("the-quick-brown-fox-jumps-over-the-lazy-dog");
+        $this->assertEquals("theQuickBrownFoxJumpsOverTheLazyDog", $result);
     }
 }
