a28b1a9ec518aa3f0fae1557e2951911cfb170d8
Igor Losev <igor.losev@gmail.com>
2023-04-01 01:14:26 +0700

    [MAJOR] Breaking changes: up minimal PHP version to 7.4; use DateTimeImmutable instead of DateTime in the DateTimeHelper.

--- src/DateTimeHelper.php
+++ src/DateTimeHelper.php
@@ -2,8 +2,16 @@
 
 namespace HBS\Helpers;
 
-use DateTime;
+use DateTimeImmutable;
 use DateTimeZone;
+
+use function abs;
+use function array_sum;
+use function intval;
+use function microtime;
+use function number_format;
+use function preg_match;
+use function sprintf;
 
 final class DateTimeHelper
 {
@@ -20,13 +28,13 @@ final class DateTimeHelper
 
     public static function convertFormatInUtc(string $date, string $inputFormat, string $outputFormat): string
     {
-        $dateTime = DateTime::createFromFormat($inputFormat, $date, new DateTimeZone(static::DATE_TIME_ZONE_UTC));
+        $dateTime = DateTimeImmutable::createFromFormat($inputFormat, $date, new DateTimeZone(self::DATE_TIME_ZONE_UTC));
         return $dateTime->format($outputFormat);
     }
 
-    public static function now(string $timeZone = self::DATE_TIME_ZONE_UTC): DateTime
+    public static function now(string $timeZone = self::DATE_TIME_ZONE_UTC): DateTimeImmutable
     {
-        return DateTime::createFromFormat(
+        return DateTimeImmutable::createFromFormat(
             'U.u',
             number_format(microtime(true), 6, '.', ''),
             new DateTimeZone($timeZone)
@@ -35,16 +43,16 @@ final class DateTimeHelper
 
     public static function parseUtcTimeOffset(string $offset): ?array
     {
-        $success = preg_match(static::UTC_TIME_OFFSET_REGEX, $offset, $matches) === 1;
+        $success = preg_match(self::UTC_TIME_OFFSET_REGEX, $offset, $matches) === 1;
 
         if (!$success) {
             return null;
         }
 
         return [
-            static::UTC_TIME_OFFSET_SIGN => $matches['sign'],
-            static::UTC_TIME_OFFSET_HOURS => intval($matches['hours']),
-            static::UTC_TIME_OFFSET_MINUTES => intval($matches['minutes']),
+            self::UTC_TIME_OFFSET_SIGN => $matches['sign'],
+            self::UTC_TIME_OFFSET_HOURS => intval($matches['hours']),
+            self::UTC_TIME_OFFSET_MINUTES => intval($matches['minutes']),
         ];
     }
 
@@ -54,46 +62,46 @@ final class DateTimeHelper
             '%s%02d:%02d:%02d',
             $seconds < 0 ? '-' : '',
             intval(abs($seconds) / 3600),
-            intval(abs($seconds) / 60 % 60),
-            intval(abs($seconds) % 60)
+            abs($seconds) / 60 % 60,
+            abs($seconds) % 60
         );
     }
 
     public static function utcTimeOffsetToSeconds(string $offset): int
     {
-        $parts = static::parseUtcTimeOffset($offset);
+        $parts = self::parseUtcTimeOffset($offset);
 
         if (!$parts) {
             return 0;
         }
 
         [
-            static::UTC_TIME_OFFSET_SIGN => $sign,
-            static::UTC_TIME_OFFSET_HOURS => $hours,
-            static::UTC_TIME_OFFSET_MINUTES => $minutes,
+            self::UTC_TIME_OFFSET_SIGN => $sign,
+            self::UTC_TIME_OFFSET_HOURS => $hours,
+            self::UTC_TIME_OFFSET_MINUTES => $minutes,
         ] = $parts;
 
-        return ($sign === '-' ? -1 : 1) * ($hours * static::HOUR + $minutes * static::MINUTE);
+        return ($sign === '-' ? -1 : 1) * ($hours * self::HOUR + $minutes * self::MINUTE);
     }
 
     public static function validateFormat(string $date, string $format = 'Y-m-d H:i:s'): bool
     {
-        $dt = DateTime::createFromFormat($format, $date);
+        $dt = DateTimeImmutable::createFromFormat($format, $date);
         return $dt !== false && !array_sum($dt::getLastErrors()) && $dt->format($format) === $date;
     }
 
     public static function validateUtcTimeOffset(string $offset): bool
     {
-        $parts = static::parseUtcTimeOffset($offset);
+        $parts = self::parseUtcTimeOffset($offset);
 
         if (!$parts) {
             return false;
         }
 
         [
-            static::UTC_TIME_OFFSET_SIGN => $sign,
-            static::UTC_TIME_OFFSET_HOURS => $hours,
-            static::UTC_TIME_OFFSET_MINUTES => $minutes,
+            self::UTC_TIME_OFFSET_SIGN => $sign,
+            self::UTC_TIME_OFFSET_HOURS => $hours,
+            self::UTC_TIME_OFFSET_MINUTES => $minutes,
         ] = $parts;
 
         if (preg_match('/^\x{00B1}$/u', $sign) === 1 && !($hours === 0 && $minutes === 0)) {
